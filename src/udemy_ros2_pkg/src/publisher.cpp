c90c8e282ad04a9e92a8d86beaac2b19bf30e7f1
fastrack <fastrack@noemail.com>
2025-08-11 12:40:13 +0700

    init ws, make, build & install publisher

---
+++ src/udemy_ros2_pkg/src/publisher.cpp
@@ -0,0 +1,37 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+class HelloWorldPubNode : public rclcpp::Node
+{
+public:
+    HelloWorldPubNode() : Node("hello_world_pub_node")
+    {
+        _publisher = create_publisher<std_msgs::msg::String>(
+            "hello_world", 10);
+
+        const auto hello_callback = [this]()
+        {
+            static auto counter = 1;
+            auto msg = std_msgs::msg::String();
+            msg.data = "Hello " + std::to_string(counter++);
+
+            _publisher->publish(msg);
+        };
+        _timer = create_wall_timer(std::chrono::seconds(1), hello_callback);
+    }
+
+private:
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _publisher;
+    rclcpp::TimerBase::SharedPtr _timer;
+};
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<HelloWorldPubNode>());
+    rclcpp::shutdown();
+    return 0;
+}
