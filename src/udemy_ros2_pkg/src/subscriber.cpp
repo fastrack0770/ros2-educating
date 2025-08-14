1228a8ca221732f85f0b4d83dd9b5bf405634c1a
fastrack <fastrack@noemail.com>
2025-08-14 11:10:36 +0700

    created subscriber

---
+++ src/udemy_ros2_pkg/src/subscriber.cpp
@@ -0,0 +1,29 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <chrono>
+
+class HelloWorldSubNode : public rclcpp::Node
+{
+public:
+    HelloWorldSubNode() : Node("hello_world_pub_node")
+    {
+        const auto callback = [](const std_msgs::msg::String & msg)
+        {
+            std::cout << msg.data << std::endl;
+        };
+
+        _subscription = create_subscription<std_msgs::msg::String>(
+            "hello_world", 10, callback);
+    }
+
+private:
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _subscription;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<HelloWorldSubNode>());
+    rclcpp::shutdown();
+    return 0;
+}
