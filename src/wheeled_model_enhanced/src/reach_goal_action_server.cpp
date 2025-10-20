7a77d678cba8313f53066656ad548c0d6c3b321a
fastrack0770 <fastrack@noemail.com>
2025-10-20 12:52:14 +0700

    Added calculation for a waypoint position. Add clang-format file - no endless strings anymore

--- src/wheeled_model_enhanced/src/reach_goal_action_server.cpp
+++ src/wheeled_model_enhanced/src/reach_goal_action_server.cpp
@@ -1,15 +1,12 @@
+#include <iomanip>
+
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-
-#include "wheeled_model_enhanced/pos.hpp"
-#include "wheeled_model_enhanced/utils.hpp"
-
-#include "wheeled_model_enhanced/action/reach_goal.hpp"
-
 #include "sensor_msgs/msg/imu.hpp"
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
-
-#include <iomanip>
+#include "wheeled_model_enhanced/action/reach_goal.hpp"
+#include "wheeled_model_enhanced/pos.hpp"
+#include "wheeled_model_enhanced/utils.hpp"
 
 using ReachGoalAction = wheeled_model_enhanced::action::ReachGoal;
 using GoalHandle = rclcpp_action::ServerGoalHandle<ReachGoalAction>;
@@ -24,69 +21,105 @@ struct Cartesian
     double z = 0.f;
 };
 
+Cartesian get_topo(const Pos &point, const Pos &point_of_view)
+{
+    // using wgs-84
+    const double B = point.latitude().value();        // latitude, rad
+    const double L = point.longitude().value();       // longitude, rad
+    const double H = point.altitude().value() / 1000; // altitude, km
+
+    const double B_view = point_of_view.latitude().value();        // latitude, rad
+    const double L_view = point_of_view.longitude().value();       // longitude, rad
+
+    // geodesic to geocentric
+    Cartesian geocentric;
+    {
+        const double f = 1 / 298.257234;
+        const double a = 6378.137; // equatorial earth radius in km
+        const double e_pow_2 = sqrt(f * (2 - f));
+        const double N = a / sqrt(1 - e_pow_2 * pow(sin(B), 2));
+
+        geocentric.x = (N + H) * cos(B) * cos(L);
+        geocentric.y = (N + H) * cos(B) * sin(L);
+        geocentric.z = (N + H - e_pow_2 * N) * sin(B);
+    }
+
+    // geocentric to topocentric
+    Cartesian topocentric;
+    {
+        /**    (-sinB * cosL   -sinB * sinL   cosB)
+         * M = ( cosB * cosL    cosB * sinL   sinB)
+         *     (-sinL           cosL          0)
+         */
+        topocentric.x = -1 * sin(B_view) * cos(L_view) * geocentric.x - sin(B_view) * sin(L_view) * geocentric.y +
+                        cos(B_view) * geocentric.z;
+        topocentric.y = cos(B_view) * cos(L_view) * geocentric.x + cos(B_view) * sin(L_view) * geocentric.y +
+                        sin(B_view) * geocentric.z;
+        topocentric.z = -1 * sin(L_view) * geocentric.x + cos(L_view) * geocentric.y;
+    }
+
+    return topocentric;
+}
+
 class ReachGoalActionServerNode : public rclcpp::Node
 {
-public:
+  public:
     ReachGoalActionServerNode() : Node("reach_goal_action_server_node")
     {
-        // navsat sub
+        // robot navsat sub
         {
-            const auto callback = [this](const sensor_msgs::msg::NavSatFix &msg)
-            {
+            const auto callback = [this](const sensor_msgs::msg::NavSatFix &msg) {
                 _robot_pos = msg;
 
                 try
                 {
-                    // using wgs-84
-                    const double B = _robot_pos.latitude().value();  // latitude, rad
-                    const double L = _robot_pos.longitude().value(); // longitude, rad
-                    const double H = _robot_pos.altitude().value() / 1000;  // altitude, km
+                    const auto topocentric = get_topo(_robot_pos, _robot_pos);
 
-                    // geodesic to geocentric
-                    Cartesian geocentric;
-                    {
-                        const double f = 1 / 298.257234;
-                        const double a = 6378.137; // equatorial earth radius in km
-                        const double e_pow_2 = sqrt(f * (2 - f));
-                        const double N = a / sqrt(1 - e_pow_2 * pow(sin(B), 2));
-
-                        geocentric.x = (N + H) * cos(B) * cos(L);
-                        geocentric.y = (N + H) * cos(B) * sin(L);
-                        geocentric.z = (N + H - e_pow_2 * N) * sin(B);
-                    }
-
-                    // geocentric to topocentric
-                    Cartesian topocentric;
-                    {
-                        /**    (-sinB * cosL   -sinB * sinL   cosB)
-                         * M = ( cosB * cosL    cosB * sinL   sinB)
-                         *     (-sinL           cosL          0)
-                         */
-                        topocentric.x = -1 * sin(B) * cos(L) * geocentric.x - sin(B) * sin(L) * geocentric.y + cos(B) * geocentric.z;
-                        topocentric.y = cos(B) * cos(L) * geocentric.x + cos(B) * sin(L) * geocentric.y + sin(B) * geocentric.z;
-                        topocentric.z = -1 * sin(L) * geocentric.x + cos(L) * geocentric.y;
-                    }
-                    RCLCPP_INFO_STREAM(get_logger(),
-                                        std::setprecision(8) <<
-                                        "lat: " << B << ", long: " << L << ", alt: " << H <<
-                                       ", geoc x: " << geocentric.x << ", y: " << geocentric.y << ", z: " << geocentric.z << 
-                                       ", topoc x: " << topocentric.x << ", y: " << topocentric.y << ", z: " << topocentric.z);
+                    RCLCPP_INFO_STREAM(get_logger(), std::setprecision(8)
+                                                         << "ROBOT lat: " << _robot_pos.latitude().value()
+                                                         << ", long: " << _robot_pos.longitude().value()
+                                                         << ", alt: " << _robot_pos.altitude().value()
+                                                         << ", topoc x: " << topocentric.x << ", y: " << topocentric.y
+                                                         << ", z: " << topocentric.z);
                 }
                 catch (const std::exception &e)
                 {
-                    RCLCPP_INFO_STREAM(get_logger(),
-                                       "Got exception while calculate topocentric coords: " << e.what());
+                    RCLCPP_INFO_STREAM(get_logger(), "Got exception while calculate topocentric coords: " << e.what());
                 }
             };
 
-            _robot_navsat_sub = create_subscription<sensor_msgs::msg::NavSatFix>(
-                "/wheeled_model_enhanced/navsat", 10, callback);
+            _robot_navsat_sub =
+                create_subscription<sensor_msgs::msg::NavSatFix>("/wheeled_model_enhanced/navsat", 10, callback);
+        }
+
+        // waypoint navsat sub
+        {
+            const auto callback = [this](const sensor_msgs::msg::NavSatFix &msg) {
+                _waypoint_pos = msg;
+
+                try
+                {
+                    const auto topocentric = get_topo(_waypoint_pos, _robot_pos);
+
+                    RCLCPP_INFO_STREAM(get_logger(), std::setprecision(8)
+                                                         << "WAYPOINT lat: " << _waypoint_pos.latitude().value()
+                                                         << ", long: " << _waypoint_pos.longitude().value()
+                                                         << ", alt: " << _waypoint_pos.altitude().value()
+                                                         << ", topoc x: " << topocentric.x << ", y: " << topocentric.y
+                                                         << ", z: " << topocentric.z);
+                }
+                catch (const std::exception &e)
+                {
+                    RCLCPP_INFO_STREAM(get_logger(), "Got exception while calculate topocentric coords: " << e.what());
+                }
+            };
+
+            _waypoint_navsat_sub = create_subscription<sensor_msgs::msg::NavSatFix>("/waypoint/navsat", 10, callback);
         }
 
         // imu sub
         {
-            const auto callback = [this](const sensor_msgs::msg::Imu &msg)
-            {
+            const auto callback = [this](const sensor_msgs::msg::Imu &msg) {
                 _robot_imu = msg;
 
                 try
@@ -100,7 +133,8 @@ public:
                     const auto psi = atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z));
 
                     RCLCPP_INFO_STREAM_THROTTLE(get_logger(), *get_clock(), 1000,
-                                                "w: " << w << ", x: " << x << ", y: " << y << ", z: " << z << ", psi: " << psi);
+                                                "w: " << w << ", x: " << x << ", y: " << y << ", z: " << z
+                                                      << ", psi: " << psi);
                 }
                 catch (const std::exception &e)
                 {
@@ -109,56 +143,51 @@ public:
                 }
             };
 
-            _imu_sub = create_subscription<sensor_msgs::msg::Imu>(
-                "/imu", 10, callback);
+            _imu_sub = create_subscription<sensor_msgs::msg::Imu>("/imu", 10, callback);
         }
 
-        const auto handle_goal = [this](const rclcpp_action::GoalUUID &, std::shared_ptr<const ReachGoalAction::Goal> goal) -> rclcpp_action::GoalResponse
-        {
+        const auto handle_goal =
+            [this](const rclcpp_action::GoalUUID &,
+                   std::shared_ptr<const ReachGoalAction::Goal> goal) -> rclcpp_action::GoalResponse {
             std::cout << "got goal "
                       << "lat: " << goal->goal_lat << " "
                       << "long: " << goal->goal_long << std::endl;
             return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
         };
-        const auto handle_cancel = [](std::shared_ptr<GoalHandle>) -> rclcpp_action::CancelResponse
-        {
+        const auto handle_cancel = [](std::shared_ptr<GoalHandle>) -> rclcpp_action::CancelResponse {
             std::cout << "goal has been cancelled" << std::endl;
 
             return rclcpp_action::CancelResponse::ACCEPT;
         };
-        const auto handle_accepted = [this](std::shared_ptr<GoalHandle> goal_handle)
-        {
-            std::thread([this, goal_handle]()
-                        {
+        const auto handle_accepted = [this](std::shared_ptr<GoalHandle> goal_handle) {
+            std::thread([this, goal_handle]() {
                 std::cout << "Execute goal" << std::endl;
 
-                            const auto goal = goal_handle->get_goal();
-                            auto feedback = std::make_shared<ReachGoalAction::Feedback>();
-                            auto result = std::make_shared<ReachGoalAction::Result>();
-                            rclcpp::Rate loop_rate(1);
+                const auto goal = goal_handle->get_goal();
+                auto feedback = std::make_shared<ReachGoalAction::Feedback>();
+                auto result = std::make_shared<ReachGoalAction::Result>();
+                rclcpp::Rate loop_rate(1);
 
-                            feedback->distance_to_point = utils::distance_in_meters(_robot_pos, Pos(_current_goal));
+                feedback->distance_to_point = utils::distance_in_meters(_robot_pos, Pos(_current_goal));
 
-                            const auto start_point = rclcpp::Clock().now();
+                const auto start_point = rclcpp::Clock().now();
 
-                            // TODO prevent an endless looping (?)
-                            while (not is_goal_reached())
-                            {
-                                feedback->distance_to_point = utils::distance_in_meters(_robot_pos, Pos(_current_goal));
+                // TODO prevent an endless looping (?)
+                while (not is_goal_reached())
+                {
+                    feedback->distance_to_point = utils::distance_in_meters(_robot_pos, Pos(_current_goal));
 
-                                goal_handle->publish_feedback(feedback);
-                                loop_rate.sleep();
-                            }
+                    goal_handle->publish_feedback(feedback);
+                    loop_rate.sleep();
+                }
 
-                            result->elapsed_time = (rclcpp::Clock().now() - start_point).seconds();
-                            goal_handle->succeed(result);
+                result->elapsed_time = (rclcpp::Clock().now() - start_point).seconds();
+                goal_handle->succeed(result);
 
-                            std::cout << "Goal Succeed" << std::endl; })
-                .detach();
+                std::cout << "Goal Succeed" << std::endl;
+            }).detach();
         };
-        _action_server = rclcpp_action::create_server<ReachGoalAction>(this, "reach_goal",
-                                                                       handle_goal,
-                                                                       handle_cancel,
+        _action_server = rclcpp_action::create_server<ReachGoalAction>(this, "reach_goal", handle_goal, handle_cancel,
                                                                        handle_accepted);
 
         std::cout << "Reach goal action server is running" << std::endl;
@@ -172,11 +201,14 @@ public:
         return utils::distance_in_meters(_robot_pos, Pos(_current_goal)) <= acceptable_range;
     }
 
-private:
+  private:
     rclcpp_action::Server<ReachGoalAction>::SharedPtr _action_server;
 
     rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr _robot_navsat_sub;
     Pos _robot_pos;
+
+    rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr _waypoint_navsat_sub;
+    Pos _waypoint_pos;
 
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr _imu_sub;
     sensor_msgs::msg::Imu _robot_imu;
